0e86e460859b45c88cb3fb54a76f2edb3e8d09ba
Abanoub Milad <abanoubcs@gmail.com>
2016-06-01 05:59:02 +0200

    semi fin 5

--- app/src/main/java/abanoubm/dayra/display/FragmentDisplayContactDetails.java
+++ app/src/main/java/abanoubm/dayra/display/FragmentDisplayContactDetails.java
@@ -15,7 +15,7 @@ import abanoubm.dayra.operations.EditContact;
 public class FragmentDisplayContactDetails extends Fragment {
     private String id = "-1";
     private int current = 0;
-    private ImageView infoImage, connImage, locImage, daysImage;
+    private ImageView[] buttons;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,27 +31,22 @@ public class FragmentDisplayContactDetails extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_display_contact, container, false);
 
-        infoImage = (ImageView) root.findViewById(R.id.infoImage);
-        connImage = (ImageView) root.findViewById(R.id.connImage);
-        locImage = (ImageView) root.findViewById(R.id.locImage);
-        daysImage = (ImageView) root.findViewById(R.id.daysImage);
+        buttons = new ImageView[]{
+                (ImageView) root.findViewById(R.id.infoImage),
+                (ImageView) root.findViewById(R.id.daysImage),
+                (ImageView) root.findViewById(R.id.connImage),
+                (ImageView) root.findViewById(R.id.editImage),
+                (ImageView) root.findViewById(R.id.locImage)
+        };
 
-        root.findViewById(R.id.editImage).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), EditContact.class).putExtra("id", id));
-            }
-        });
-        infoImage.setOnClickListener(new View.OnClickListener() {
+        buttons[0].setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 if (current != 0) {
+                    buttons[current].setBackgroundColor(0);
                     current = 0;
-                    infoImage.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
-                    connImage.setBackgroundColor(0);
-                    locImage.setBackgroundColor(0);
-                    daysImage.setBackgroundColor(0);
+                    buttons[0].setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
 
                     Bundle arguments = new Bundle();
                     arguments.putString("id", id);
@@ -66,16 +61,36 @@ public class FragmentDisplayContactDetails extends Fragment {
 
             }
         });
-        connImage.setOnClickListener(new View.OnClickListener() {
+        buttons[1].setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 if (current != 1) {
+                    buttons[current].setBackgroundColor(0);
                     current = 1;
-                    infoImage.setBackgroundColor(0);
-                    connImage.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
-                    locImage.setBackgroundColor(0);
-                    daysImage.setBackgroundColor(0);
+                    buttons[1].setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
+
+                    Bundle arguments = new Bundle();
+                    arguments.putString("id", id);
+
+                    FragmentDisplayContactDay fragment = new FragmentDisplayContactDay();
+                    fragment.setArguments(arguments);
+
+                    getChildFragmentManager().beginTransaction()
+                            .replace(R.id.display_contact_fragment, fragment)
+                            .commit();
+                }
+            }
+        });
+        buttons[2].setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (current != 2) {
+                    buttons[current].setBackgroundColor(0);
+                    current = 2;
+                    buttons[2].setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
+
 
                     Bundle arguments = new Bundle();
                     arguments.putString("id", id);
@@ -90,16 +105,16 @@ public class FragmentDisplayContactDetails extends Fragment {
 
             }
         });
-        locImage.setOnClickListener(new View.OnClickListener() {
+
+        buttons[3].setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                if (current != 2) {
-                    current = 2;
-                    infoImage.setBackgroundColor(0);
-                    connImage.setBackgroundColor(0);
-                    locImage.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
-                    daysImage.setBackgroundColor(0);
+                if (current != 3) {
+                    buttons[current].setBackgroundColor(0);
+                    current = 3;
+                    buttons[3].setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
+
 
                     Bundle arguments = new Bundle();
                     arguments.putString("id", id);
@@ -114,29 +129,22 @@ public class FragmentDisplayContactDetails extends Fragment {
 
             }
         });
-        daysImage.setOnClickListener(new View.OnClickListener() {
+
+        buttons[4].setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                if (current != 3) {
-                    current = 3;
-                    infoImage.setBackgroundColor(0);
-                    connImage.setBackgroundColor(0);
-                    locImage.setBackgroundColor(0);
-                    daysImage.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
+                if (current != 4) {
+                    buttons[current].setBackgroundColor(0);
+                    current = 4;
+                    buttons[4].setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.colorAccent));
+                    startActivity(new Intent(getActivity(), EditContact.class).putExtra("id", id));
 
-                    Bundle arguments = new Bundle();
-                    arguments.putString("id", id);
-
-                    FragmentDisplayContactDay fragment = new FragmentDisplayContactDay();
-                    fragment.setArguments(arguments);
-
-                    getChildFragmentManager().beginTransaction()
-                            .replace(R.id.display_contact_fragment, fragment)
-                            .commit();
                 }
+
             }
         });
+
 
         if (savedInstanceState == null) {
             Bundle arguments = new Bundle();
